b94ed25c588eaa99e0df82244022dcb23b45a94e
Kingsley Omokwe <thisletterk@Kingsleys-MacBook-Air.local>
2022-08-28 19:19:16 +0100

    ecommerce created using vite.js

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import HomePage from "./HomePage";
+
+function App() {
+	return (
+		<div>
+			<HomePage />
+		</div>
+	);
+}
+
+export default App;
